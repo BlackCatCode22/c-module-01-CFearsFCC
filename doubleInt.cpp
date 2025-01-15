685fc4342d855918b3702c9e02c94b92a4c221bf
CFearsFCC <CFears1@MY.SCCCD.EDU>
2025-01-14 17:19:49 -0800

    Create doubleInt.cpp

---
+++ doubleInt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+int loop = 1;
+
+int y;
+
+int doubleInt(int x)
+{
+    return 2*x;
+}
+
+
+int main()
+{
+    while (loop == 1)
+    {
+        std::cout << "**Please enter an integer between 1-10** \n";
+
+        std::cin >> y;
+
+        if (y >= 1 and y <= 10)
+        {
+            std::cout << "your number is = " << y << std::endl;
+            std::cout << "your number doubled is = " << doubleInt(y) << std::endl;
+            std::cout << "Play again?  \n" << std::endl;
+
+        }
+        else
+        {
+            std::cout << "**Invalid. Please enter an integer between 1-10** \n";
+        }
+
+    }
+
+
+}
+
+
